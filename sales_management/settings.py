9cb46a12726582948f22dddf6e4e94081c6b7b30
Sha-703 <shrlongo@gmail.com>
2025-05-24 11:12:28 +0100

    ajout du nom de domaine

--- sales_management/settings.py
+++ sales_management/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your-secret-key'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
