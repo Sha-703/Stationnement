466a353e7c74ba8940632daab7ba8a5b5c77be8b
Sha-703 <shrlongo@gmail.com>
2025-05-24 11:05:38 +0100

    ajout du nom de domaine

--- sales_management/settings.py
+++ sales_management/settings.py
@@ -22,7 +22,12 @@ SECRET_KEY = 'your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.43.163']
+ALLOWED_HOSTS = [
+    'stationnement.onrender.com',
+    'localhost',
+    '127.0.0.1',
+    '192.168.43.163',
+]
 
 # Application definition
 INSTALLED_APPS = [
