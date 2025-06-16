810b9197a72f3098e98c71bc8f4c628e85dfcd4e
Sha-703 <shrlongo@gmail.com>
2025-06-16 20:22:47 +0100

    ajout page 404

--- sales_management/urls.py
+++ sales_management/urls.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404
+from django.shortcuts import render
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
+
+handler404 = 'sales_management.urls.custom_404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
